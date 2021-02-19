1fdeaea331c3ea8b9b7a1ada08ef235c2ee7c4bb
Kundan Gupta <kundankumar8377@gmail.com>
2021-02-19 11:37:15 +0530

    [feat] Add user authentication

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv').config();
 const { Keystone } = require('@keystonejs/keystone');
 const { GraphQLApp } = require('@keystonejs/app-graphql');
 const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
 const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
 const PROJECT_NAME = 'Keystone Blog';
 const adapterConfig = {
@@ -19,10 +20,23 @@ const keystone = new Keystone({
 keystone.createList('Post', PostSchema);
 keystone.createList('User', UserSchema);
 
+const authStrategy = keystone.createAuthStrategy({
+  type: PasswordAuthStrategy,
+  list: 'User',
+  config: {
+    identityField: 'email',
+    secretField: 'password',
+  },
+});
+
 module.exports = {
   keystone,
   apps: [
     new GraphQLApp(),
-    new AdminUIApp({ name: PROJECT_NAME, enableDefaultRoute: true }),
+    new AdminUIApp({
+      name: PROJECT_NAME,
+      enableDefaultRoute: true,
+      authStrategy,
+    }),
   ],
 };
